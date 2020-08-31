cb44e1414a0366756cd3928b8177073c595ff291
stud <stud@Notebook-bib>
2020-08-31 09:13:29 +0200

    Hello World!!!

---
+++ Hello World!!!/src/Car.java
@@ -0,0 +1,47 @@
+
+public class Car {
+	
+public String farbe;
+public String getMarke() {
+	return marke;
+}
+
+public void setMarke(String marke) {
+	this.marke = marke;
+}
+
+public String getModelle() {
+	return modelle;
+}
+
+public void setModelle(String modelle) {
+	this.modelle = modelle;
+}
+
+public int getLeistung() {
+	return Leistung;
+}
+
+public void setLeistung(int leistung) {
+	Leistung = leistung;
+}
+
+private String marke;
+private String modelle;
+private int Leistung;
+
+public void fahren() {
+	
+	System.out.println("Das Auto fährt...................");
+
+
+}
+
+public String getFarbe() {
+	return farbe;
+}
+
+public void setFarbe(String farbe) {
+	this.farbe = farbe;
+}
+}
---
+++ Hello World!!!/src/Program.java
@@ -0,0 +1,14 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Car Auto = new Car();
+		Auto.fahren();
+		Auto.setFarbe("Gelb");
+		
+		System.out.println(Auto.farbe);
+
+	}
+
+}
